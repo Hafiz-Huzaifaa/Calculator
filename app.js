3034fdd00e6bf3d7bd68f4fa01460e4e9ccf88e2
Haiz-Huzaifaa <hhuzaifa80@gmail.com>
2025-02-03 14:03:49 +0500

    calculator 1st project

---
+++ app.js
@@ -0,0 +1,101 @@
+let input = document.getElementById("input");
+
+function operators (secondValue){
+    let firstValue = input.innerHTML;
+
+    if (firstValue.startsWith("0")){
+        input.innerHTML = secondValue;
+    }else{
+        input.innerHTML = firstValue + secondValue;
+    }
+}
+
+function perform (){
+    let result = input.innerHTML;
+    input.innerHTML = eval(result)
+};
+
+function allClear (){
+    input.innerHTML = "0"
+};
+
+function clearing (){
+    let deleteValue = input.innerHTML;
+    input.innerHTML = deleteValue.slice(0,-1);
+    input.innerHTML = "0"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function operators(currentValue) {
+//     let previousValue = input.innerHTML;
+//     if (previousValue.startsWith("0")) {
+//         input.innerHTML = currentValue;
+//     }else{
+//         input.innerHTML = previousValue + currentValue;
+//     }
+    
+// }
+
+// // for equal to
+
+// function perform() {
+//     let result = input.innerHTML;
+//     input.innerHTML = eval(result);
+
+
+// }
+
+// function allClear() {
+//     input.innerHTML = "0";
+// }
+
+// function clearing (){
+//     let deleteValue = input.innerHTML;
+//     if(deleteValue > 0 ){
+//         input.innerHTML = deleteValue.slice(0, -1);
+//     }
+// }
